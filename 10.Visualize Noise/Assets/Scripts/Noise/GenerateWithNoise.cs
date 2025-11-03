d1ad9f01ade24cf718871eb31f8ea13475b3078b
JohanStraede <FR53WX@student.aau.dk>
2025-11-03 11:44:07 +0100

    player

--- 10.Visualize Noise/Assets/Scripts/Noise/GenerateWithNoise.cs
+++ 10.Visualize Noise/Assets/Scripts/Noise/GenerateWithNoise.cs
@@ -31,7 +31,7 @@ public class GenerateWithNoise : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             recalculateWorld();
 
